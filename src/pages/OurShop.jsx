c941114ba7a894f38e94bdccfc15b6f9672e9a2b
Redoan Hasan <redoanhasan57@gmail.com>
2025-04-22 01:32:58 +0600

    have to create server file

--- src/pages/OurShop.jsx
+++ src/pages/OurShop.jsx
@@ -2,18 +2,22 @@ import React from "react";
 import OurMenuBannerOne from "../Components/OurMenuPage/OurMenuBannerOne";
 import MenuTabs from "../Components/ourShop/MenuTabs";
 import { useParams } from "react-router";
+import { Helmet } from "react-helmet-async";
 
 const OurShop = () => {
-  const {category} = useParams();
-    console.log(category);
+  const { category } = useParams();
+  console.log(category);
   return (
     <div>
+      <Helmet>
+        <title>Bistro Boss | Our Shop</title>
+      </Helmet>
       <OurMenuBannerOne
         bgImgUrl={"/assets/shop/banner2.jpg"}
         title={"Our Shop"}
         subtitle={"Would you like to try a dish?"}
       />
-      <MenuTabs category={category}/>
+      <MenuTabs category={category} />
     </div>
   );
 };
