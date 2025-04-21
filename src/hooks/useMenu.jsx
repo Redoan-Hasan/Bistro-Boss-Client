5ad3f2f58e59c68d2c44b042d2a8f6f3e4cd181e
Redoan Hasan <redoanhasan57@gmail.com>
2025-04-22 02:35:08 +0600

    menu data fetching from api

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const useMenu = (filterCategoryName) => {
     const [menuItems, setMenuItems] = useState([]);
     useEffect(()=>{
-        fetch('/menu.json')
+        fetch('http://localhost:5000/menu')
         .then(res => res.json())
         .then(data => setMenuItems(data.filter(item => item.category === filterCategoryName)));
     },[filterCategoryName])
