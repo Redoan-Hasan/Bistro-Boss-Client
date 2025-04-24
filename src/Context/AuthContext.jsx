9eb1cbd029d310ed51aa81d627de6d9531253687
Redoan Hasan <redoanhasan57@gmail.com>
2025-04-25 03:51:17 +0600

    useAuth hook setup done

--- src/Context/AuthContext.jsx
+++ src/Context/AuthContext.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { app } from "../firebase/firebase.config";
 
 // eslint-disable-next-line react-refresh/only-export-components
@@ -9,7 +9,36 @@ const auth = getAuth(app);
 const AuthContext = ({children}) => {
     const [user , setUser] = useState(null);
     const [loading, setLoading] = useState(true);
-    const authInfo = {user, loading}
+
+    // login 
+    const login = (email,password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // logout 
+    const logout = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+    // register 
+    const register = (email,password) =>{
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(()=>{
+        const unsubcribe = onAuthStateChanged(auth, currentUser =>{
+            setUser(currentUser);
+            setLoading(false);
+        })
+        return () => unsubcribe(); 
+    }, [])
+
+
+
+    const authInfo = {user, loading , register , login , logout};
     return (
         <Context.Provider value={authInfo}>
             {children}
